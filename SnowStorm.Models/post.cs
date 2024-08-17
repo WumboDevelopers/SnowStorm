efbb8d1f4a55bf07549f7fbe0d48ffa1adbea42b
Chlove <155773111+ch-love@users.noreply.github.com>
2024-08-17 15:48:40 -0400

    Create post.cs

---
+++ SnowStorm.Models/post.cs
@@ -0,0 +1,11 @@
+namespace WumboBackend.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public List<Reply> Replies { get; set; } = new List<Reply>();
+    }
+}
